2754a6b5585d7dd63903c737dff2f0f3c08ce2a2
ChandanaVaidya <chandana.vaidya@niveussolutions.com>
2024-11-25 10:58:45 +0530

    created async funtion to write to a file

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
-const myPromise = new Promise((resolve,reject)=>{
-    const success = true;
-    if(success){
-        resolve("Hello this message is of promise!");
-    }
-    else{
-        reject("Something is wrong!");
-    }
+const fs = require('fs').promises;
+
+// async function to write to file
+async function writeToFile() {
+  try {
+    await fs.writeFile('msg.txt', 'This is to say that a message is written using fs and async/await.');
+    console.log('File has been saved');
+  } catch (error) {
+    console.log('Error writing a file:', error);
+  }
+}
+// Promise to display message
+const myPromise = new Promise((resolve, reject) => {
+  const success = true;
+  if (success) {
+    resolve("Hello, this is a message from promise!!!");
+  } else {
+    reject("Something is wrong!!!");
+  }
 });
 
 myPromise
-.then((message)=>{
+  .then((message) => {
     console.log(message);
-})
-.catch((Error)=>{
+    // Calling async funtion to write to the text file
+    writeToFile();
+  })
+  .catch((error) => {
     console.log(error);
-
-});
+  });
